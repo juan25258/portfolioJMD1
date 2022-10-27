1d9a936cd95d8dff88ec2b842020e82f85b9138b
juan25258 <juan25257@gmail.com>
2022-10-27 15:43:44 -0300

    algo

--- src/app/componentes/barra-superior/barra-superior.component.ts
+++ src/app/componentes/barra-superior/barra-superior.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-barra-superior',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarraSuperiorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos();
   }
 
 }
